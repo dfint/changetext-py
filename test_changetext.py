02a449d9ba9030038c017bacad4d4d900b278b6f
Insolor <Insolor@localhost>
2016-02-21 01:53:26 +0300

    Move false case triggering tests of corr_tags() into test_changetext.py

--- test_changetext.py
+++ test_changetext.py
@@ -3,6 +3,13 @@ import pytest
 from changetext import ChangeText
 
 
+def test_not_tags():
+    # проверка ложных срабатываний:
+    assert ChangeText("<-") == None
+    assert ChangeText("<1в") == None
+    assert ChangeText(" <> ") == None
+
+
 def test_tag_wrap():
     ChangeText('whatever <gent>')
     assert ChangeText('голова') == 'головы'
