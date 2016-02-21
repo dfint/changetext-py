faf3f1df9fc9ba1bb3c65ae7ce3e4530ede07fe7
Mike Manturov <insolor@gmail.com>
2016-02-21 20:37:57 +0300

    Add test for corr_contextual()

--- test_changetext.py
+++ test_changetext.py
@@ -27,3 +27,15 @@ def test_capitalize_tag():
 def test_corr_color_of_color():
     assert ChangeText("цвет серебристого цвета") == "серебристый цвет"
     assert ChangeText("цвет цвета морской волны") == "цвет морской волны"
+
+
+def test_corr_contextual():
+    ChangeText('  Dwarf Fortress  ')  # switch to the 'main' context
+    assert ChangeText('Повар') in {'Повар', None}
+    assert ChangeText('Рыба') in {'Рыба', None}
+    
+    ChangeText('Овощи/фрукты/листья')  # switch to the 'kitchen' context
+    assert ChangeText('Повар') == 'Готовить'
+    
+    ChangeText('Граждане (10)')  # switch to the 'units' context
+    assert ChangeText('Рыба') == 'Рыбачить'
