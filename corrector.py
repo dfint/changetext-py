696521e48a99646a403a77a44027440fc1b08b66
insolor <insolor@gmail.com>
2021-10-27 21:17:05 +0300

    Add basic Corrector implementation

---
+++ corrector.py
@@ -0,0 +1,53 @@
+import re
+
+
+class Corrector:
+    def __init__(self):
+        self.preliminary_changes = list()
+        self.final_changes = list()
+
+    def preliminary_change(self, regex=None, predicate=None):
+        def decorator(func):
+            nonlocal regex, predicate
+
+            if regex is not None:
+                if isinstance(regex, str):
+                    regex = re.compile(regex)
+
+                predicate = lambda text: regex.search(text)
+
+            assert predicate is not None
+            self.preliminary_changes.append((predicate, func))
+
+            return func
+        return decorator
+
+    def final_change(self, regex=None, predicate=None):
+        def decorator(func):
+            nonlocal regex, predicate
+
+            if regex is not None:
+                if isinstance(regex, str):
+                    regex = re.compile(regex)
+
+                predicate = lambda text: regex.search(text)
+
+            assert predicate is not None
+            self.final_changes.append((predicate, func))
+
+            return func
+        return decorator
+
+    def change_text(self, text):
+        result = None
+        for predicate, func in self.preliminary_changes:
+            predicate_result = predicate(text)
+            if predicate_result:
+                result = func(text, predicate_result) or result
+                if result:
+                    text = result
+
+        for predicate, func in self.final_changes:
+            predicate_result = predicate(text)
+            if predicate_result:
+                return func(text, predicate_result)
