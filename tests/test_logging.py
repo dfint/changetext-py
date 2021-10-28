168ea03133e4c51a97054919a939709ce4e8433e
insolor <insolor@gmail.com>
2021-10-28 09:52:40 +0300

    Add a test for the log_exceptions_and_result decorator

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -1,6 +1,7 @@
 import io
+import contextlib
 
-from changetext import get_logger
+from changetext import get_logger, log_exceptions_and_result
 
 
 def test_cache():
@@ -19,3 +20,16 @@ def test_double_write():
     # Try to write the same again
     get_logger(file).write(text, result)
     assert file.getvalue() == "{!r} --> {!r}\n".format(text, result)
+
+
+def test_exception_logging():
+    stderr = io.StringIO()
+    with contextlib.redirect_stderr(stderr):
+        @log_exceptions_and_result
+        def foo(_):
+            raise ValueError
+
+        foo('text')
+
+    stderr_text = stderr.getvalue().splitlines(keepends=False)
+    assert stderr_text[0] == "An error occurred."
