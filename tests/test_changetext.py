e89b3bac74de7a6c0e1638e4087f22c919223ab8
insolor <insolor@gmail.com>
2022-06-25 17:13:16 +0300

    Move test of contextual.py to test_contextual.py

--- tests/test_changetext.py
+++ tests/test_changetext.py
@@ -14,18 +14,6 @@ def test_corr_color_of_color(text, expected):
     assert change_text(text) == expected
 
 
-def test_corr_contextual():
-    change_text("  Dwarf Fortress  ")  # switch to the 'main' context
-    assert change_text("Повар") in {"Повар", None}
-    assert change_text("Рыба") in {"Рыба", None}
-
-    change_text("Овощи/фрукты/листья")  # switch to the 'kitchen' context
-    assert change_text("Повар") == "Готовить"
-
-    change_text("Граждане (10)")  # switch to the 'units' context
-    assert change_text("Рыба") == "Рыбачить"
-
-
 @pytest.mark.parametrize(
     "text, expected",
     [
