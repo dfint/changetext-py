80b90a5660c92f93cbb0f45c8b298b9286746a83
insolor <insolor@gmail.com>
2022-06-25 14:17:46 +0300

    Use parametrization in tests

--- tests/test_changetext.py
+++ tests/test_changetext.py
@@ -1,9 +1,17 @@
+import pytest
+
 from changetext import change_text
 
 
-def test_corr_color_of_color():
-    assert change_text("цвет серебристого цвета") == "серебристый цвет"
-    assert change_text("цвет цвета морской волны") == "цвет морской волны"
+@pytest.mark.parametrize(
+    "text, expected",
+    [
+        ("цвет серебристого цвета", "серебристый цвет"),
+        ("цвет цвета морской волны", "цвет морской волны"),
+    ]
+)
+def test_corr_color_of_color(text, expected):
+    assert change_text(text) == expected
 
 
 def test_corr_contextual():
@@ -18,10 +26,16 @@ def test_corr_contextual():
     assert change_text("Рыба") == "Рыбачить"
 
 
-def test_corr_has_verb():
+@pytest.mark.parametrize(
+    "text, expected",
+    [
+        (" имеет создал ", " создал "),
+        (" был создать ", " создал "),
+        (" имеет пришёл ", " пришёл "),
+        (" имеет упал ", " упал "),
+        (" имеет стрямкал ", " стрямкал "),
+    ]
+)
+def test_corr_has_verb(text, expected):
     # Test 'has' + verb fix
-    assert change_text(" имеет создал ") == " создал "
-    assert change_text(" был создать ") == " создал "
-    assert change_text(" имеет пришёл ") == " пришёл "
-    assert change_text(" имеет упал ") == " упал "
-    assert change_text(" имеет стрямкал ") == " стрямкал "
+    assert change_text(text) == expected
